0bdd430302ea588f04cd050f05eea4ac2c3eb046
Martron64 <jmarcosgan@gmail.com>
2026-01-01 14:58:47 -0300

    Update CubCaveJS.js

--- CubCaveJS.js
+++ CubCaveJS.js
@@ -1,7 +1,8 @@
-funtion calcular(){
+function calcular(){
     let dificultad = 42;
 
   let elemento = document.getElementById("resultado");
 
   elemento.textContent = "Dificultad: " + dificultad;
 }
+
