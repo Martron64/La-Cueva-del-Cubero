cd17822fa4b633c9186261e605b4f2eb19ce8e36
Martron64 <jmarcosgan@gmail.com>
2026-01-01 22:37:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 function calcular(){
-    let dificultad = 42;
-
-  let elemento = document.getElementById("resultado");
-
-  elemento.textContent = "Dificultad: " + dificultad;
+let tabla = document.getElementById("tablaCubos");
+let fila = tabla.insertRow();
+fila.insertCell().textContent ="3X3";
+fila.insertCell().textContent ="26";
+fila.insertCell().textContent ="3";
+fila.insertCell().textContent ="1";
+fila.insertCell().textContent ="6";
+fila.insertCell().textContent ="1";
+fila.insertCell().textContent ="1";
+fila.insertCell().textContent ="7";
+fila.insertCell().textContent ="6";
+fila.insertCell().textContent ="12";
+fila.insertCell().textContent ="21";
+fila.insertCell().textContent ="0";
+fila.insertCell().textContent ="0";
+fila.insertCell().textContent ="0";
+fila.insertCell().textContent ="NO SE";
 }
+
 
