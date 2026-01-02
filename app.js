cf634ddca1a6bbee96252e4226990ea4def7a29e
Martron64 <jmarcosgan@gmail.com>
2026-01-02 14:37:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function calcular(){
 let tabla = document.getElementById("tablaCubos");
-let fila = tabla.insertRow();//NO SE
+let fila = tabla.insertRow();
 fila.insertCell().textContent ="3X3";
 fila.insertCell().textContent ="26";
 fila.insertCell().textContent ="3";
@@ -18,9 +18,31 @@ fila.insertCell().textContent ="0";
 fila.insertCell().textContent ="NO SE";
 }
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("HOLA")  
+  
   actualizar();
 });
+function actualizar(){
+  console.log("HOLA");
+  
+let tabla = document.getElementById("tablaCubos");
+let fila = tabla.insertRow();
+fila.insertCell().textContent ="3X3";
+fila.insertCell().textContent ="26";
+fila.insertCell().textContent ="3";
+fila.insertCell().textContent ="1";
+fila.insertCell().textContent ="6";
+fila.insertCell().textContent ="1";
+fila.insertCell().textContent ="1";
+fila.insertCell().textContent ="7";
+fila.insertCell().textContent ="6";
+fila.insertCell().textContent ="12";
+fila.insertCell().textContent ="21";
+fila.insertCell().textContent ="0";
+fila.insertCell().textContent ="0";
+fila.insertCell().textContent ="0";
+fila.insertCell().textContent ="NO SE";
+}
+
 
 
 
