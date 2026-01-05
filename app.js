300580f51de16caa1770aa78477bc9b14adac0d2
Martron64 <jmarcosgan@gmail.com>
2026-01-05 16:15:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -150,7 +150,57 @@ function ImprimirTabla(data,algoritmos){
   fila.insertCell().textContent =data[x].difAlgTot+((2.718281828**data[x].difBlock)-1)+((2.718281828**data[x].difDeform)-1)+data[x].difParid;
 }
 }
+function encontrar(contador,elemento){
+  let x;
+  for(x=0;x<contador.length;x++){
+    if(contador[x].pasos==elemento)
+      return x;
+  }
+  return x;
+}
 
+function calculoDifAlgoritmica(algoritmo){
+  let contador=[];
+  for(let x=0;x<algoritmo.pasos.length;x++){
+    let id=encontrar(contador,algoritmo.pasos[x]);
+    if (id==contador.length){
+      let estructura={pasos:algoritmo.pasos[x],contador:1};
+      contador.push(estructura);
+    }
+    else {
+      contador[id].contador=contador[id].contador+1;
+    }
+  }
+  let max=contador[0].contador;
+  let min=max;
+  for(let x=1;x<contador.length;x++){
+    if(contador[x].contador>max)
+      max=contador[x].contador;
+    if(contador[x].contador<min)
+      min=contador[x].contador;
+  }
+  return (contador.length*max/min);
+}
+
+function dificultadAlgoritmicaPromedio(algoritmos){
+  let contador=0;
+  for(let x=0;x<algoritmos.lengt;x++){
+    contador=contador+calculoDifAlgoritmica(algoritmos[x]);
+  }
+  return (contador/algoritmos.lengt);
+}
+
+function dificultadAlgoritmicaTotal(algoritmos){
+  let contador_original=0;
+  let contador_espejo=0;
+  for(let x=0;x<algoritmos.length;x++){
+    if(algoritmo[x].tipo=="original")
+      contador_original=contador_original+calculoDifAlgoritmica(algoritmos[x]);
+    else
+      contador_espejo=contador_espejo+calculoDifAlgoritmica(algoritmos[x]);
+  }
+  return (contador_original**2+contador_espejo/2);
+}
 
 
 
